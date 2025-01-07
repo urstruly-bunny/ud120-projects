042874b86316e19331692b2bdbb942ef032f6ad2
urstruly-bunny <tejabhargav0@gmail.com>
2025-01-07 10:25:59 +0000

    check

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -17,7 +17,10 @@ from email_preprocess import preprocess
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
 ### labels_train and labels_test are the corresponding item labels
-features_train, features_test, labels_train, labels_test = preprocess()
+features_train, features_test, labels_train, labels_test = preprocess(
+    words_file="/workspaces/ud120-projects/tools/word_data.pkl",
+    authors_file="/workspaces/ud120-projects/tools/email_authors.pkl"
+)
 
 
 #########################################################
@@ -37,3 +40,21 @@ The Code Below solves that issue, So use this one
 # labels_train = labels_train[:int(len(labels_train)/100)]
 
 #########################################################
+#features_train = features_train[:int(len(features_train)/100)]
+#labels_train = labels_train[:int(len(labels_train)/100)]
+
+from sklearn import svm
+from sklearn.metrics import accuracy_score
+clf = svm.SVC(kernel='rbf', C=10000)
+t0 = time()
+clf.fit(features_train, labels_train)
+print("Training Time:", round(time()-t0, 3), "s")
+t1 = time()
+pred = clf.predict(features_test)
+print("Prediction Time:", round(time()-t1, 3), "s")
+
+
+acc=clf.score(features_test,labels_test)
+print(acc)
+import numpy as np
+print(np.count(pred=1))
