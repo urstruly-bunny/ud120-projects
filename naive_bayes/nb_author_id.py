7089f81ecb15a08ba0948da5a86aebc100f64709
urstruly-bunny <tejabhargav0@gmail.com>
2025-01-07 09:26:19 +0000

    nvb

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -19,7 +19,11 @@ from email_preprocess import preprocess
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
 ### labels_train and labels_test are the corresponding item labels
-features_train, features_test, labels_train, labels_test = preprocess()
+features_train, features_test, labels_train, labels_test = preprocess(
+    words_file="/workspaces/ud120-projects/tools/word_data.pkl",
+    authors_file="/workspaces/ud120-projects/tools/email_authors.pkl"
+)
+
 
 
 ##############################################################
@@ -35,6 +39,22 @@ You Will be Required to record time for Training and Predicting
 The Code Given on Udacity Website is in Python-2
 The Following Code is Python-3 version of the same code
 '''
+from sklearn.naive_bayes import GaussianNB
+clf=GaussianNB()
+
+t0 = time()
+# # < your clf.fit() line of code >
+clf.fit(features_train,labels_train)
+print("Training Time:", round(time()-t0, 3), "s")
+
+t0 = time()
+# # < your clf.predict() line of code >
+print(clf.predict(features_test))
+print("Predicting Time:", round(time()-t0, 3), "s")
+
+
+acc=clf.score(features_test,labels_test)
+print(acc)
 
 # t0 = time()
 # # < your clf.fit() line of code >
