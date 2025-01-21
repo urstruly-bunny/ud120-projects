2f3f9f9077563581a37f8967d6087c7e1fb4a5dc
urstruly-bunny <tejabhargav0@gmail.com>
2025-01-21 05:21:52 +0000

    selection

--- feature_selection/find_signature.py
+++ feature_selection/find_signature.py
@@ -3,13 +3,14 @@
 import joblib
 import numpy
 numpy.random.seed(42)
+from sklearn import tree
 
 
 ### The words (features) and authors (labels), already largely processed.
 ### These files should have been created from the previous (Lesson 10)
 ### mini-project.
-words_file = "../text_learning/your_word_data.pkl" 
-authors_file = "../text_learning/your_email_authors.pkl"
+words_file = "feature_selection/word_data_overfit.pkl" 
+authors_file = "feature_selection/email_authors_overfit.pkl"
 word_data = joblib.load( open(words_file, "r"))
 authors = joblib.load( open(authors_file, "r") )
 
@@ -38,5 +39,7 @@ labels_train   = labels_train[:150]
 
 ### your code goes here
 
-
+clf=tree.DecisionTreeClassifier()
+clf.fit(features_train,labels_train)
+print(clf.score(features_test,labels_test))
 
